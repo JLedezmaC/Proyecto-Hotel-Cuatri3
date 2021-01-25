2e34a398f04089053607ae5dd2c2031ac3932550
mariaconejo <mary03081@gmail.com>
2021-01-24 18:37:19 -0600

    Se agrego estilos y js para la galeria

--- js/script.js
+++ js/script.js
@@ -44,3 +44,30 @@ for (let i = 0; i < enlaces.length; i++) {
   // Oculta todos los elementos, excepto el primero.
   // Se usa el método substring(1) para eliminar el primer caracter ('#').
 updateVisibleContent(enlaces[0].getAttribute('href').substring(1));
+
+//Variables que obtinenen los enlaces de miniaturas y el div contenedor con la imagen en grande
+const links = document.querySelectorAll('.miniature');
+const bigImage = document.getElementById('bigImage');
+
+//Funcion principal de la Galeria
+for (let i = 0; i < links.length; i++){ 
+    links[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        //De esta forma se abstrae la ubicacion de una imagen y se modifica en el div grande (src)
+        let url = event.currentTarget.getAttribute('src');
+        bigImage.src = `${url}`;
+
+        //De esta forma se aplica un estilo para cada miniatura en donde se da click
+        for(let i = 0 ; i < links.length; i++){
+            links[i].classList.add('img-content-hidden');
+            if (links[i].getAttribute('src') == url){   
+                links[i].classList.remove('img-content-hidden');
+            }
+        }
+        for(let i = 0; i < bigImage.length; i++){
+            bigImage[i].classList.remove('img-active');
+        }
+        event.currentTarget.classList.add('img-active');
+    });
+    
+}
